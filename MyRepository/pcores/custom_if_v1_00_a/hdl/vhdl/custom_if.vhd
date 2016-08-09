3441b548a275ba82c559214a5dddc7236d3b2e8d
Mohd Amiruddin Zainol <mohd.amiruddin.zainol@gmail.com>
2016-08-09 13:09:03 +0100

    Add files via upload

---
+++ MyRepository/pcores/custom_if_v1_00_a/hdl/vhdl/custom_if.vhd
@@ -0,0 +1,354 @@
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+--This library is free software; you can redistribute it and/or
+--modify it under the terms of the GNU Lesser General Public
+--License as published by the Free Software Foundation; either
+--version 2.1 of the License, or (at your option) any later version.
+--
+--This library is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+--Lesser General Public License for more details.
+--
+--You should have received a copy of the GNU Lesser General Public
+--License along with this library; if not, write to the Free Software
+--Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+--
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+--
+-- Author: 			Mohd Amiruddin Zainol (mohd.a.zainol@gmail.com)
+-- Entity: 			custom_if.vhd
+-- Version:			1.0
+-- Description: This file is a custom interface for CPCIe
+--
+-- Additional Comments:
+--
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+library proc_common_v3_00_a;
+use proc_common_v3_00_a.proc_common_pkg.all;
+use proc_common_v3_00_a.ipif_pkg.all;
+
+library axi_lite_ipif_v1_01_a;
+use axi_lite_ipif_v1_01_a.axi_lite_ipif;
+
+library custom_if_v1_00_a;
+use custom_if_v1_00_a.user_logic;
+
+entity custom_if is
+  generic
+  (
+    C_S_AXI_DATA_WIDTH             : integer              := 32;
+    C_S_AXI_ADDR_WIDTH             : integer              := 32;
+    C_S_AXI_MIN_SIZE               : std_logic_vector     := X"000001FF";
+    C_USE_WSTRB                    : integer              := 0;
+    C_DPHASE_TIMEOUT               : integer              := 8;
+    C_BASEADDR                     : std_logic_vector     := X"FFFFFFFF";
+    C_HIGHADDR                     : std_logic_vector     := X"00000000";
+    C_FAMILY                       : string               := "virtex6";
+    C_NUM_REG                      : integer              := 1;
+    C_NUM_MEM                      : integer              := 1;
+    C_SLV_AWIDTH                   : integer              := 32;
+    C_SLV_DWIDTH                   : integer              := 32
+  );
+  port
+  (
+		HOST_rst : IN std_logic;
+		HOST_clk : IN std_logic;
+		HOST_w_data : IN std_logic_vector(31 downto 0);
+		HOST_w_wren : IN std_logic;
+		HOST_r_rden : IN std_logic;
+		HOST_r_data : OUT std_logic_vector(31 downto 0);
+		HOST_w_full : OUT std_logic;
+		HOST_r_empty : OUT std_logic;
+		HOST_mem_clk : IN std_logic;
+		HOST_mem_addr : IN std_logic_vector(4 downto 0);
+		HOST_mem_w_data : IN std_logic_vector(31 downto 0);
+		HOST_mem_r_data : OUT std_logic_vector(31 downto 0);
+		HOST_mem_wren : IN std_logic;
+		S_AXIS_TDATA : IN std_logic_vector(31 downto 0);
+		S_AXIS_TLAST : IN std_logic;
+		S_AXIS_TVALID : IN std_logic;
+		S_AXIS_TREADY : OUT std_logic;
+		M_AXIS_TREADY : IN std_logic;
+		M_AXIS_TVALID : OUT std_logic;
+		M_AXIS_TDATA : OUT std_logic_vector(31 downto 0);
+		M_AXIS_TLAST : OUT std_logic;
+    S_AXI_ACLK                     : in  std_logic;
+    S_AXI_ARESETN                  : in  std_logic;
+    S_AXI_AWADDR                   : in  std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);
+    S_AXI_AWVALID                  : in  std_logic;
+    S_AXI_WDATA                    : in  std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
+    S_AXI_WSTRB                    : in  std_logic_vector((C_S_AXI_DATA_WIDTH/8)-1 downto 0);
+    S_AXI_WVALID                   : in  std_logic;
+    S_AXI_BREADY                   : in  std_logic;
+    S_AXI_ARADDR                   : in  std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);
+    S_AXI_ARVALID                  : in  std_logic;
+    S_AXI_RREADY                   : in  std_logic;
+    S_AXI_ARREADY                  : out std_logic;
+    S_AXI_RDATA                    : out std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
+    S_AXI_RRESP                    : out std_logic_vector(1 downto 0);
+    S_AXI_RVALID                   : out std_logic;
+    S_AXI_WREADY                   : out std_logic;
+    S_AXI_BRESP                    : out std_logic_vector(1 downto 0);
+    S_AXI_BVALID                   : out std_logic;
+    S_AXI_AWREADY                  : out std_logic
+    -- DO NOT EDIT ABOVE THIS LINE ---------------------
+  );
+
+  attribute MAX_FANOUT : string;
+  attribute SIGIS : string;
+  attribute MAX_FANOUT of S_AXI_ACLK       : signal is "10000";
+  attribute MAX_FANOUT of S_AXI_ARESETN       : signal is "10000";
+  attribute SIGIS of S_AXI_ACLK       : signal is "Clk";
+  attribute SIGIS of S_AXI_ARESETN       : signal is "Rst";
+
+end entity custom_if;
+
+------------------------------------------------------------------------------
+-- Architecture section
+------------------------------------------------------------------------------
+
+architecture IMP of custom_if is
+
+COMPONENT fifo_async is
+  PORT (
+    rst : IN STD_LOGIC;
+    wr_clk : IN STD_LOGIC;
+    rd_clk : IN STD_LOGIC;
+    din : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    wr_en : IN STD_LOGIC;
+    rd_en : IN STD_LOGIC;
+    dout : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    full : OUT STD_LOGIC;
+    empty : OUT STD_LOGIC
+  );
+END COMPONENT;
+
+  constant USER_SLV_DWIDTH                : integer              := C_S_AXI_DATA_WIDTH;
+
+  constant IPIF_SLV_DWIDTH                : integer              := C_S_AXI_DATA_WIDTH;
+
+  constant ZERO_ADDR_PAD                  : std_logic_vector(0 to 31) := (others => '0');
+  constant USER_SLV_BASEADDR              : std_logic_vector     := C_BASEADDR;
+  constant USER_SLV_HIGHADDR              : std_logic_vector     := C_HIGHADDR;
+
+  constant IPIF_ARD_ADDR_RANGE_ARRAY      : SLV64_ARRAY_TYPE     :=
+    (
+      ZERO_ADDR_PAD & USER_SLV_BASEADDR,  -- user logic slave space base address
+      ZERO_ADDR_PAD & USER_SLV_HIGHADDR   -- user logic slave space high address
+    );
+
+  constant USER_SLV_NUM_REG               : integer              := 1;
+  constant USER_NUM_REG                   : integer              := USER_SLV_NUM_REG;
+  constant TOTAL_IPIF_CE                  : integer              := USER_NUM_REG;
+
+  constant IPIF_ARD_NUM_CE_ARRAY          : INTEGER_ARRAY_TYPE   :=
+    (
+      0  => (USER_SLV_NUM_REG)            -- number of ce for user logic slave space
+    );
+
+  ------------------------------------------
+  -- Index for CS/CE
+  ------------------------------------------
+  constant USER_SLV_CS_INDEX              : integer              := 0;
+  constant USER_SLV_CE_INDEX              : integer              := calc_start_ce_index(IPIF_ARD_NUM_CE_ARRAY, USER_SLV_CS_INDEX);
+
+  constant USER_CE_INDEX                  : integer              := USER_SLV_CE_INDEX;
+
+  ------------------------------------------
+  -- IP Interconnect (IPIC) signal declarations
+  ------------------------------------------
+  signal ipif_Bus2IP_Clk                : std_logic;
+  signal ipif_Bus2IP_Resetn             : std_logic;
+  signal ipif_Bus2IP_Addr               : std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);
+  signal ipif_Bus2IP_RNW                : std_logic;
+  signal ipif_Bus2IP_BE                 : std_logic_vector(IPIF_SLV_DWIDTH/8-1 downto 0);
+  signal ipif_Bus2IP_CS                 : std_logic_vector((IPIF_ARD_ADDR_RANGE_ARRAY'LENGTH)/2-1 downto 0);
+  signal ipif_Bus2IP_RdCE               : std_logic_vector(calc_num_ce(IPIF_ARD_NUM_CE_ARRAY)-1 downto 0);
+  signal ipif_Bus2IP_WrCE               : std_logic_vector(calc_num_ce(IPIF_ARD_NUM_CE_ARRAY)-1 downto 0);
+  signal ipif_Bus2IP_Data               : std_logic_vector(IPIF_SLV_DWIDTH-1 downto 0);
+  signal ipif_IP2Bus_WrAck              : std_logic;
+  signal ipif_IP2Bus_RdAck              : std_logic;
+  signal ipif_IP2Bus_Error              : std_logic;
+  signal ipif_IP2Bus_Data               : std_logic_vector(IPIF_SLV_DWIDTH-1 downto 0);
+  signal user_Bus2IP_RdCE               : std_logic_vector(USER_NUM_REG-1 downto 0);
+  signal user_Bus2IP_WrCE               : std_logic_vector(USER_NUM_REG-1 downto 0);
+  signal user_IP2Bus_Data               : std_logic_vector(USER_SLV_DWIDTH-1 downto 0);
+  signal user_IP2Bus_RdAck              : std_logic;
+  signal user_IP2Bus_WrAck              : std_logic;
+  signal user_IP2Bus_Error              : std_logic;
+
+signal FIFO_INPUT_rst			: std_logic;
+signal FIFO_INPUT_wr_en			: std_logic;
+signal FIFO_INPUT_rd_en			: std_logic;
+signal FIFO_INPUT_din			: std_logic_vector(31 downto 0);
+signal FIFO_INPUT_dout			: std_logic_vector(31 downto 0);
+signal FIFO_INPUT_full			:  std_logic;
+signal FIFO_INPUT_empty			:  std_logic;
+
+signal FIFO_OUTPUT_rst			: std_logic;
+signal FIFO_OUTPUT_wr_en		: std_logic;
+signal FIFO_OUTPUT_rd_en		: std_logic;
+signal FIFO_OUTPUT_din			: std_logic_vector(31 downto 0);
+signal FIFO_OUTPUT_dout			: std_logic_vector(31 downto 0);
+signal FIFO_OUTPUT_full			:  std_logic;
+signal FIFO_OUTPUT_empty		:  std_logic;
+
+signal empty_d1 					: std_logic;
+signal empty_d2 					: std_logic;
+
+begin
+
+fifo_32_input : fifo_async
+  PORT MAP (
+    rst => HOST_rst,
+    wr_clk => HOST_clk,
+    rd_clk => S_AXI_ACLK,
+    din => FIFO_INPUT_din,
+    wr_en => FIFO_INPUT_wr_en,
+    rd_en => FIFO_INPUT_rd_en,
+    dout => FIFO_INPUT_dout,
+    full => FIFO_INPUT_full,
+    empty => FIFO_INPUT_empty
+  );
+
+fifo_32_output : fifo_async
+  PORT MAP (
+    rst => HOST_rst,
+    wr_clk => S_AXI_ACLK,
+    rd_clk => HOST_clk,
+    din => FIFO_OUTPUT_din,
+    wr_en => FIFO_OUTPUT_wr_en,
+    rd_en => FIFO_OUTPUT_rd_en,
+    dout => FIFO_OUTPUT_dout,
+    full => FIFO_OUTPUT_full,
+    empty => FIFO_OUTPUT_empty
+  );
+
+FIFO_OUTPUT_din		<= S_AXIS_TDATA;
+FIFO_OUTPUT_wr_en		<= S_AXIS_TVALID;
+S_AXIS_TREADY			<= not FIFO_OUTPUT_full;
+FIFO_OUTPUT_rd_en 	<= HOST_r_rden;
+HOST_r_data				<= FIFO_OUTPUT_dout;
+HOST_r_empty 			<= FIFO_OUTPUT_empty;
+
+FIFO_INPUT_din 		<= HOST_w_data;
+FIFO_INPUT_wr_en 		<= HOST_w_wren;
+HOST_w_full				<= FIFO_INPUT_full;
+FIFO_INPUT_rd_en		<= M_AXIS_TREADY and (not empty_d2);
+M_AXIS_TDATA 			<= FIFO_INPUT_dout;
+M_AXIS_TLAST			<= '0';
+
+  process (S_AXI_ACLK) is
+  begin
+    if S_AXI_ACLK'event and S_AXI_ACLK = '1' then
+      if(HOST_rst = '1') then
+        empty_d1 <= '1';
+        empty_d2 <= '1';
+      else
+        empty_d1 <= FIFO_INPUT_empty;
+        empty_d2 <= empty_d1;
+		  M_AXIS_TVALID <= not(FIFO_INPUT_empty or empty_d2);
+      end if;
+    end if;
+  end process;
+
+  ------------------------------------------
+  -- instantiate axi_lite_ipif
+  ------------------------------------------
+  AXI_LITE_IPIF_I : entity axi_lite_ipif_v1_01_a.axi_lite_ipif
+    generic map
+    (
+      C_S_AXI_DATA_WIDTH             => IPIF_SLV_DWIDTH,
+      C_S_AXI_ADDR_WIDTH             => C_S_AXI_ADDR_WIDTH,
+      C_S_AXI_MIN_SIZE               => C_S_AXI_MIN_SIZE,
+      C_USE_WSTRB                    => C_USE_WSTRB,
+      C_DPHASE_TIMEOUT               => C_DPHASE_TIMEOUT,
+      C_ARD_ADDR_RANGE_ARRAY         => IPIF_ARD_ADDR_RANGE_ARRAY,
+      C_ARD_NUM_CE_ARRAY             => IPIF_ARD_NUM_CE_ARRAY,
+      C_FAMILY                       => C_FAMILY
+    )
+    port map
+    (
+      S_AXI_ACLK                     => S_AXI_ACLK,
+      S_AXI_ARESETN                  => S_AXI_ARESETN,
+      S_AXI_AWADDR                   => S_AXI_AWADDR,
+      S_AXI_AWVALID                  => S_AXI_AWVALID,
+      S_AXI_WDATA                    => S_AXI_WDATA,
+      S_AXI_WSTRB                    => S_AXI_WSTRB,
+      S_AXI_WVALID                   => S_AXI_WVALID,
+      S_AXI_BREADY                   => S_AXI_BREADY,
+      S_AXI_ARADDR                   => S_AXI_ARADDR,
+      S_AXI_ARVALID                  => S_AXI_ARVALID,
+      S_AXI_RREADY                   => S_AXI_RREADY,
+      S_AXI_ARREADY                  => S_AXI_ARREADY,
+      S_AXI_RDATA                    => S_AXI_RDATA,
+      S_AXI_RRESP                    => S_AXI_RRESP,
+      S_AXI_RVALID                   => S_AXI_RVALID,
+      S_AXI_WREADY                   => S_AXI_WREADY,
+      S_AXI_BRESP                    => S_AXI_BRESP,
+      S_AXI_BVALID                   => S_AXI_BVALID,
+      S_AXI_AWREADY                  => S_AXI_AWREADY,
+      Bus2IP_Clk                     => ipif_Bus2IP_Clk,
+      Bus2IP_Resetn                  => ipif_Bus2IP_Resetn,
+      Bus2IP_Addr                    => ipif_Bus2IP_Addr,
+      Bus2IP_RNW                     => ipif_Bus2IP_RNW,
+      Bus2IP_BE                      => ipif_Bus2IP_BE,
+      Bus2IP_CS                      => ipif_Bus2IP_CS,
+      Bus2IP_RdCE                    => ipif_Bus2IP_RdCE,
+      Bus2IP_WrCE                    => ipif_Bus2IP_WrCE,
+      Bus2IP_Data                    => ipif_Bus2IP_Data,
+      IP2Bus_WrAck                   => ipif_IP2Bus_WrAck,
+      IP2Bus_RdAck                   => ipif_IP2Bus_RdAck,
+      IP2Bus_Error                   => ipif_IP2Bus_Error,
+      IP2Bus_Data                    => ipif_IP2Bus_Data
+    );
+
+  ------------------------------------------
+  -- instantiate User Logic
+  ------------------------------------------
+  USER_LOGIC_I : entity custom_if_v1_00_a.user_logic
+    generic map
+    (
+      C_NUM_REG                      => USER_NUM_REG,
+      C_SLV_DWIDTH                   => USER_SLV_DWIDTH
+    )
+    port map
+    (
+      CLK_P2C                        => HOST_mem_clk,
+      ADDR_P2C								 => HOST_mem_addr,
+      DI_P2C								 => HOST_mem_w_data,
+      DO_P2C								 => HOST_mem_r_data,
+      WE_P2C  								 => HOST_mem_wren,
+      Bus2IP_Clk                     => ipif_Bus2IP_Clk,
+      Bus2IP_Resetn                  => ipif_Bus2IP_Resetn,
+      Bus2IP_Addr                    => ipif_Bus2IP_Addr,
+      Bus2IP_CS                      => ipif_Bus2IP_CS,
+      Bus2IP_RNW                     => ipif_Bus2IP_RNW,
+      Bus2IP_Data                    => ipif_Bus2IP_Data,
+      Bus2IP_BE                      => ipif_Bus2IP_BE,
+      Bus2IP_RdCE                    => user_Bus2IP_RdCE,
+      Bus2IP_WrCE                    => user_Bus2IP_WrCE,
+      IP2Bus_Data                    => user_IP2Bus_Data,
+      IP2Bus_RdAck                   => user_IP2Bus_RdAck,
+      IP2Bus_WrAck                   => user_IP2Bus_WrAck,
+      IP2Bus_Error                   => user_IP2Bus_Error
+    );
+
+  ------------------------------------------
+  -- connect internal signals
+  ------------------------------------------
+  ipif_IP2Bus_Data <= user_IP2Bus_Data;
+  ipif_IP2Bus_WrAck <= user_IP2Bus_WrAck;
+  ipif_IP2Bus_RdAck <= user_IP2Bus_RdAck;
+  ipif_IP2Bus_Error <= user_IP2Bus_Error;
+
+  user_Bus2IP_RdCE <= ipif_Bus2IP_RdCE(USER_NUM_REG-1 downto 0);
+  user_Bus2IP_WrCE <= ipif_Bus2IP_WrCE(USER_NUM_REG-1 downto 0);
+
+end IMP;
