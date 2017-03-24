0d6b506d5886274b373c5a6f7058ddca3f94652b
Mohd Amiruddin Zainol <mohd.amiruddin.zainol@gmail.com>
2017-03-24 15:02:53 +0000

    Update xpro_c_controller.vhd

--- ip_repo/xmatchpro/c.srcs/sources_1/c_ctlr/c_ctlr.srcs/xpro_c_controller.vhd
+++ ip_repo/xmatchpro/c.srcs/sources_1/c_ctlr/c_ctlr.srcs/xpro_c_controller.vhd
@@ -1,3 +1,29 @@
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+--This library is free software; you can redistribute it and/or
+--modify it under the terms of the GNU Lesser General Public
+--License as published by the Free Software Foundation; either
+--version 2.1 of the License, or (at your option) any later version.
+--
+--This library is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+--Lesser General Public License for more details.
+--
+--You should have received a copy of the GNU Lesser General Public
+--License along with this library; if not, write to the Free Software
+--Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+--
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+--
+-- Author: 			Mohd Amiruddin Zainol (mohd.a.zainol@gmail.com)
+-- Entity: 			xpro_c_controller.vhd
+-- Version:			1.0
+-- Description: 		X-MatchPRO Compressor Controller
+--
+-- Additional Comments:
+--
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
