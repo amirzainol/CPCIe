c2b2e7bb5389fe78dd2b33fb4f26a6f390311b7c
Mohd Amiruddin Zainol <mohd.amiruddin.zainol@gmail.com>
2018-05-20 13:09:28 +0100

    Add files via upload

---
+++ PROJECT_FILES/standalone/x_vc707/src/mysystem2_wrapper.vhd
@@ -0,0 +1,88 @@
+--Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2014.4 (win64) Build 1071353 Tue Nov 18 18:29:27 MST 2014
+--Date        : Tue Oct 24 12:24:46 2017
+--Host        : IT002223 running 64-bit Service Pack 1  (build 7601)
+--Command     : generate_target mysystem2_wrapper.bd
+--Design      : mysystem2_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity mysystem2_wrapper is
+  port (
+    ddr3_sdram_addr : out STD_LOGIC_VECTOR ( 13 downto 0 );
+    ddr3_sdram_ba : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    ddr3_sdram_cas_n : out STD_LOGIC;
+    ddr3_sdram_ck_n : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_ck_p : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_cke : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_cs_n : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_dm : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    ddr3_sdram_dq : inout STD_LOGIC_VECTOR ( 63 downto 0 );
+    ddr3_sdram_dqs_n : inout STD_LOGIC_VECTOR ( 7 downto 0 );
+    ddr3_sdram_dqs_p : inout STD_LOGIC_VECTOR ( 7 downto 0 );
+    ddr3_sdram_odt : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_ras_n : out STD_LOGIC;
+    ddr3_sdram_reset_n : out STD_LOGIC;
+    ddr3_sdram_we_n : out STD_LOGIC;
+    reset : in STD_LOGIC;
+    rs232_uart_rxd : in STD_LOGIC;
+    rs232_uart_txd : out STD_LOGIC;
+    sys_diff_clock_clk_n : in STD_LOGIC;
+    sys_diff_clock_clk_p : in STD_LOGIC
+  );
+end mysystem2_wrapper;
+
+architecture STRUCTURE of mysystem2_wrapper is
+  component mysystem2 is
+  port (
+    ddr3_sdram_dq : inout STD_LOGIC_VECTOR ( 63 downto 0 );
+    ddr3_sdram_dqs_p : inout STD_LOGIC_VECTOR ( 7 downto 0 );
+    ddr3_sdram_dqs_n : inout STD_LOGIC_VECTOR ( 7 downto 0 );
+    ddr3_sdram_addr : out STD_LOGIC_VECTOR ( 13 downto 0 );
+    ddr3_sdram_ba : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    ddr3_sdram_ras_n : out STD_LOGIC;
+    ddr3_sdram_cas_n : out STD_LOGIC;
+    ddr3_sdram_we_n : out STD_LOGIC;
+    ddr3_sdram_reset_n : out STD_LOGIC;
+    ddr3_sdram_ck_p : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_ck_n : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_cke : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_cs_n : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_dm : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    ddr3_sdram_odt : out STD_LOGIC_VECTOR ( 0 to 0 );
+    sys_diff_clock_clk_p : in STD_LOGIC;
+    sys_diff_clock_clk_n : in STD_LOGIC;
+    rs232_uart_rxd : in STD_LOGIC;
+    rs232_uart_txd : out STD_LOGIC;
+    reset : in STD_LOGIC
+  );
+  end component mysystem2;
+begin
+mysystem2_i: component mysystem2
+    port map (
+      ddr3_sdram_addr(13 downto 0) => ddr3_sdram_addr(13 downto 0),
+      ddr3_sdram_ba(2 downto 0) => ddr3_sdram_ba(2 downto 0),
+      ddr3_sdram_cas_n => ddr3_sdram_cas_n,
+      ddr3_sdram_ck_n(0) => ddr3_sdram_ck_n(0),
+      ddr3_sdram_ck_p(0) => ddr3_sdram_ck_p(0),
+      ddr3_sdram_cke(0) => ddr3_sdram_cke(0),
+      ddr3_sdram_cs_n(0) => ddr3_sdram_cs_n(0),
+      ddr3_sdram_dm(7 downto 0) => ddr3_sdram_dm(7 downto 0),
+      ddr3_sdram_dq(63 downto 0) => ddr3_sdram_dq(63 downto 0),
+      ddr3_sdram_dqs_n(7 downto 0) => ddr3_sdram_dqs_n(7 downto 0),
+      ddr3_sdram_dqs_p(7 downto 0) => ddr3_sdram_dqs_p(7 downto 0),
+      ddr3_sdram_odt(0) => ddr3_sdram_odt(0),
+      ddr3_sdram_ras_n => ddr3_sdram_ras_n,
+      ddr3_sdram_reset_n => ddr3_sdram_reset_n,
+      ddr3_sdram_we_n => ddr3_sdram_we_n,
+      reset => reset,
+      rs232_uart_rxd => rs232_uart_rxd,
+      rs232_uart_txd => rs232_uart_txd,
+      sys_diff_clock_clk_n => sys_diff_clock_clk_n,
+      sys_diff_clock_clk_p => sys_diff_clock_clk_p
+    );
+end STRUCTURE;
